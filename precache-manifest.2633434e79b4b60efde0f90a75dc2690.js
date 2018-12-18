0c2f26b585f77d4b9539fbe4bb976588b9480e6d
Steve N <nguy.steve@gmail.com>
2018-12-17 22:21:46 -0800

    Updates

---
+++ precache-manifest.2633434e79b4b60efde0f90a75dc2690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e910aa7af4ad6ffd90",
+    "url": "/clickygame/static/css/main.e59ad77f.chunk.css"
+  },
+  {
+    "revision": "59e910aa7af4ad6ffd90",
+    "url": "/clickygame/static/js/main.59e910aa.chunk.js"
+  },
+  {
+    "revision": "e441f2fd4897c51e2574",
+    "url": "/clickygame/static/js/1.e441f2fd.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "01c87c68ed29be95c9868668ccfb9f02",
+    "url": "/clickygame/index.html"
+  }
+];
